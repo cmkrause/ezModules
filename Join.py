dba67ac4d6188ddd529d86d64dcc49adf08727fa
cmkt4f <cmkt4f@mail.missouri.edu>
2016-10-05 18:23:02 -0500

    Fix joining with large datasets
    Data is now chunked to facilitate the joining of large datasets without
    running out of memory

--- Join.py
+++ Join.py
@@ -2,13 +2,18 @@ import arcpy
 from ez.Misc import *
 from ez.Describe import *
 from ez.FC import *
+from ez.Timer import *
 
 def joinTableToFC(inputTable, inputFC, joinField,
                   outputFC = None,
-                  keepFieldsTable = "ALL", keepFieldsFC = "ALL"):
+                  keepFieldsTable = "ALL", keepFieldsFC = "ALL",
+                  chunkSize = None):
+
+    timer = ezTimer()
 
     inputTableView = randomString()
     arcpy.MakeTableView_management(inputTable, inputTableView)
+
 
     if type(joinField) is str:
         joinFieldTable  = joinField
@@ -25,15 +30,6 @@ def joinTableToFC(inputTable, inputFC, joinField,
     if joinFieldTable not in keepFieldsTable:
         keepFieldsTable.insert(0, joinFieldTable)
 
-    tableData = {}
-
-    tableCursor = arcpy.da.SearchCursor(inputTableView, keepFieldsTable)
-    for row in tableCursor:
-        joinValue    = row[0]
-        data         = row[1:]
-        tableData[joinValue] = data
-    del tableCursor
-
     ## In the case where no outputFC is specified, the data will be appended
     ## to the existing inputFC
     if outputFC == None:
@@ -47,23 +43,59 @@ def joinTableToFC(inputTable, inputFC, joinField,
                 fieldsToCopy.remove(existingField)
         ## Create those fields using the fields in inputTable as a template
         copyFieldsToFC(inputTable, outputFC, fieldsToCopy)
+
+    timer.update("Creating fields for join data")
+
     
-    fcCursor = arcpy.da.UpdateCursor(outputFC, keepFieldsTable)
-    for row in fcCursor:
-        joinValue = row[0]
-        if joinValue in tableData:
-            data = tableData[joinValue]
-            ## Concatenation of tuples explained:
-            ## http://stackoverflow.com/a/8538676/6214388
-            updatedRow = (joinValue, ) + data
-            fcCursor.updateRow(updatedRow)
-    del fcCursor
+
+    with arcpy.da.SearchCursor(inputTableView, keepFieldsTable) as tableCursor:
+        
+        if chunkSize == None:
+            chunkSize = 10**9
+            
+        chunks = chunkIterable(tableCursor, chunkSize)
+        
+        for tableCursorChunk in chunks:
+            tableData = {}
+            
+            for row in tableCursorChunk:
+                joinValue    = row[0]
+                data         = row[1:]
+                tableData[joinValue] = data
+
+            timer.update("Creating in-memory dictionary")
+
+            
+            
+            fcCursor = arcpy.da.UpdateCursor(outputFC, keepFieldsTable)
+            for row in fcCursor:
+                joinValue = row[0]
+                if joinValue in tableData:
+                    data = tableData[joinValue]
+                    ## Concatenation of tuples explained:
+                    ## http://stackoverflow.com/a/8538676/6214388
+                    updatedRow = (joinValue, ) + data
+                    fcCursor.updateRow(updatedRow)
+            del fcCursor
+
+            timer.update("Joining with update cursor")
+
+            timer.report()
     
     return True
 
 if __name__ == "__main__":
+    
+    
+##    inputTable = "C:\\NHGIS\\NHGIS_0008_2010.csv"
+##    inputFC = "C:\\NHGIS\\Test.gdb\\Blocks"
+##
+##    joinTableToFC(inputTable, inputFC, "GISJOIN",
+##                        keepFieldsTable=["CM1AA", "CM1AB", "CM1AC", "CM1AD", "CM1AE", "CM1AF", "CM1AG"])
 
-    inputTable = "C:\\NHGIS\\NHGIS_0008_2010.csv"
-    inputFC = "C:\\NHGIS\\NHGIS2010.gdb\\Blockgroups"
+    
+    inputTable = "C:\\NHGIS\\2010BlockPopulation.csv"
+    inputFC = "C:\\NHGIS\\Test.gdb\\BlocksTest"
 
-    print joinTableToFC(inputTable, inputFC, "GISJOIN", keepFieldsTable=["CM1AA", "CM1AB", "CM1AC", "CM1AD", "CM1AE", "CM1AF", "CM1AG"])
+    print joinTableToFC(inputTable, inputFC, "GISJOIN",
+                        keepFieldsTable=["H7V001"], chunkSize = (12*(10**6)))
