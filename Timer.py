1f88c4c4ca9ab624fa630dced832453d334be7ba
cmkt4f <cmkt4f@mail.missouri.edu>
2017-08-11 11:25:25 -0400

    Added support for running in ArcMap or Pro

--- Timer.py
+++ Timer.py
@@ -3,13 +3,18 @@ from ez.Table import *
 
 class ezTimer:
 
-    def __init__(self, title = "", realtimePrint = False, style = "AsciiTable"):
+    def __init__(self, title = "", realtimePrint = False, style = "AsciiTable", arcToolbox = False):
         self.reportData = []
         self.headers = ["Total Time", "Step Time", "Step Description"]
         self.title = title
         self.realtimePrint = realtimePrint
         self.printedHeader = False
         self.style = style
+
+        ## https://stackoverflow.com/a/34179809
+        self.arcToolbox = arcToolbox
+        if self.arcToolbox == True:
+            self.arcpy = __import__('arcpy')
 
         self.timeZero = datetime.now()
         self.lastTime = self.timeZero
@@ -26,14 +31,19 @@ class ezTimer:
         self.lastTime = currentTime
 
         if self.realtimePrint == True:
-            if self.printedHeader == False:
-                reportTable = ezTable(self.reportData, self.headers, title = self.title, display = False, style = self.style)
-                self.printedHeader = True
+            if self.arcToolbox == False:
+                if self.printedHeader == False:
+                    reportTable = ezTable(self.reportData, self.headers, title = self.title, display = False, style = self.style)
+                    self.printedHeader = True
+                else:
+                    reportTable = ezTable([reportRow], display = False, style = self.style)
+                print reportTable
             else:
-                reportTable = ezTable([reportRow], display = False, style = self.style)
-            print reportTable
-            return reportTable
-
+                if self.printedHeader == False:
+                    self.arcpy.AddMessage("\t\t".join([str(value) for value in self.headers]))
+                    self.printedHeader = True
+                self.arcpy.AddMessage("\t".join([str(value) for value in reportRow]))
+            
     def report(self, display = True, outputFile = None):
         if len(self.reportData) > 0:
             reportTable = ezTable(self.reportData, self.headers, title = self.title, display = False, style = self.style)
