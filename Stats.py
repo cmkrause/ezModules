5c909f0181bf3aa53971d86f50d55945b55dbe5f
cmkt4f <cmkt4f@mail.missouri.edu>
2017-08-12 15:44:12 -0400

    Added runningStats

--- Stats.py
+++ Stats.py
@@ -3,10 +3,46 @@ import arcpy
 import os
 from copy import copy
 from ez.Misc import *
-from ez.Describe import *
+##from ez.Describe import *
 from ez.Table import *
 from scipy.stats import chi2_contingency
 from numpy import percentile
+
+## This class is from https://github.com/liyanage/python-modules/blob/master/running_stats.py
+## It was originally based on http://www.johndcook.com/standard_deviation.html
+class runningStats:
+
+    def __init__(self):
+        self.n = 0
+        self.old_m = 0
+        self.new_m = 0
+        self.old_s = 0
+        self.new_s = 0
+    
+    def clear(self):
+        self.n = 0
+        
+    def push(self, x):
+        self.n += 1
+        
+        if self.n == 1:
+            self.old_m = self.new_m = x
+            self.old_s = 0
+        else:
+            self.new_m = self.old_m + (x - self.old_m) / self.n
+            self.new_s = self.old_s + (x - self.old_m) * (x - self.new_m)
+            
+            self.old_m = self.new_m
+            self.old_s = self.new_s
+
+    def mean(self):
+        return self.new_m if self.n else 0.0
+    
+    def variance(self):
+        return self.new_s / (self.n - 1) if self.n > 1 else 0.0
+        
+    def stdDev(self):
+        return math.sqrt(self.variance())
 
 
 ## This function creates percentage columns
