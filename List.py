e3952d3b811a43fdd6b342991b2101ea59016b3d
cmkt4f <cmkt4f@mail.missouri.edu>
2017-11-02 20:44:09 -0400

    Added listFeatureClasses

--- List.py
+++ List.py
@@ -1,5 +1,9 @@
 import arcpy, os
 from ez.Contexts import Environment
+from arcpy import env
+
+## Need to handle the case where env.workspace is not set and
+## path == None
 
 def listDatasets(wildcard = None, featureType = None, path = None):
     if path == None:
@@ -28,8 +32,54 @@ def listDatasets(wildcard = None, featureType = None, path = None):
 
         return fullPathDatasets
 
-def listFCs(path):
+def listFeatureClasses(wildcard = None, featureType = None, path = None):
+    if path == None:
+        if arcpy.Describe(env.workspace).dataType == "FeatureDataset":
+            path = os.path.dirname(env.workspace)
+            featureDataset = os.path.basename(env.workspace)
+            kwargs = {"feature_dataset":featureDataset}
+        else:
+            featureDataset = None
+            kwargs = {}
+            
+        with Environment("workspace", path):
+            if wildcard <> None and featureType <> None:
+                featureClasses = arcpy.ListFeatureClasses(wildcard, featureType, **kwargs)
+            elif wildcard == None and featureType <> None:
+                featureClasses = arcpy.ListFeatureClasses(feature_type = featureType, **kwargs)
+            elif wildcard <> None and featureType == None:
+                featureClasses = arcpy.ListFeatureClasses(wild_card = wildcard, **kwargs)
+            elif wildcard == None and featureType == None:
+                featureClasses = arcpy.ListFeatureClasses(**kwargs)
 
+        return featureClasses
+    else:
+        if arcpy.Describe(path).dataType == "FeatureDataset":
+            featureDataset = os.path.basename(path)
+            path = os.path.dirname(path)
+            kwargs = {"feature_dataset":featureDataset}
+        else:
+            featureDataset = None
+            kwargs = {}
+
+        with Environment("workspace", path):
+            if wildcard <> None and featureType <> None:
+                featureClasses = arcpy.ListFeatureClasses(wildcard, featureType, **kwargs)
+            elif wildcard == None and featureType <> None:
+                featureClasses = arcpy.ListFeatureClasses(feature_type = featureType, **kwargs)
+            elif wildcard <> None and featureType == None:
+                featureClasses = arcpy.ListFeatureClasses(wild_card = wildcard, **kwargs)
+            elif wildcard == None and featureType == None:
+                featureClasses = arcpy.ListFeatureClasses(**kwargs)
+
+        fullPathFeatureClasses = []
+        for featureClass in featureClasses:
+            if featureDataset == None:
+                fullPathFeatureClasses.append(os.path.join(path, featureClass))
+            else:
+                fullPathFeatureClasses.append(os.path.join(path, featureDataset, featureClass))
+
+        return fullPathFeatureClasses
     return
 
 def listFiles(path):
